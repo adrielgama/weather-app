02c52726716b6b7a8436533f4d8e9b52757add52
Adriel Gama <adrielgama@gmail.com>
2022-08-07 00:23:42 -0300

    feat(index): improve start project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+import "./reset.css";
+
+function App() {
+  return (
+    <Container>
+      <div className="App">
+        <h1>Weather APP</h1>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
